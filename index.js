603e933d05784cdff361e74f3f776c627d9d5815
nihaltowfiq <nihaltowfiq@gmail.com>
2020-11-16 19:18:31 +0600

    addNewApartment problem, data did not send to DB

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('apartmentImage'));
+app.use(fileUpload());
 
 const port = 5500;
 
@@ -21,11 +24,34 @@ client.connect(err => {
   app.post('/bookings', (req, res) => {
 
   });
-  
-  const apartmentCollection = client.db("apartmentHunt").collection("apartments");
-  app.post('/addHouse', (req, res) => {
 
-  });
+  const apartmentCollection = client.db("apartmentHunt").collection("apartments");
+
+  app.post('/addNewApartment', (req, res) => {
+    console.log(req);
+    const file = req.files.file;
+    const title = req.body.title;
+    const price = req.body.price;
+    const location = req.body.location;
+    const bedroom = req.body.bedroom;
+    const bathroom = req.body.bathroom;
+
+    const newImg = file.data;
+    const encImg = newImg.toString('base64');
+
+    const image = {
+      contentType: file.mimetype,
+      size: file.size,
+      img: Buffer.from(encImg, 'base64')
+    };
+
+    apartmentCollection.insertOne({ title, price, location, bedroom, bathroom, image })
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      }).catch(function (error) {
+        // Handle error
+      });
+  })
 
 });
 
@@ -35,4 +61,4 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(port);
+app.listen(process.env.PORT || port);
