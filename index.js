69ee51b22dbdf4d876c84a5419bcbe2a0684ab5a
nihaltowfiq <nihaltowfiq@gmail.com>
2020-11-17 01:06:57 +0600

    bug fixed and GET method added

--- index.js
+++ index.js
@@ -13,27 +13,43 @@ app.use(fileUpload());
 const port = 5500;
 
 //dbPasss: xyx12345, userName team09   dbName: apartmentHunt;
+//Nihal mongoDB => user:apartmentHuntUser, pass: abcd1234, DB-Name: apartmentHunt;
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://team09:xyx12345@cluster0.sppky.mongodb.net/apartmentHunt?retryWrites=true&w=majority";
+const uri = "mongodb+srv://apartmentHuntUser:abcd1234@cluster0.pf9xm.mongodb.net/apartmentHunt?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://team09:xyx12345@cluster0.sppky.mongodb.net/apartmentHunt?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
   console.log('db connected');
   const bookingCollection = client.db("apartmentHunt").collection("bookings");
+
   app.post('/addBooking', (req, res) => {
     console.log(req);
     const name = req.body.name;
     const number = req.body.number;
-    const email = req.body.email;    
+    const email = req.body.email;
     const message = req.body.message;
 
     bookingCollection.insertOne({ name, number, email, message })
       .then(result => {
         res.send(result.insertedCount > 0)
-      }).catch(function (error) {
-        // Handle error
+      })
+      .catch(error => {
+        console.log(error);
       });
+  });
+
+  app.get('/bookings', (req, res) => {
+    bookingCollection.find({ email: req.query.email })
+      .toArray((err, docs) => {
+        res.send(docs)
+      })
+  });
+
+  app.get('/allBookings', (req, res) => {
+    bookingCollection.find({})
+      .toArray((err, docs) => res.send(docs));
   });
 
   const apartmentCollection = client.db("apartmentHunt").collection("apartments");
@@ -59,10 +75,16 @@ client.connect(err => {
     apartmentCollection.insertOne({ title, price, location, bedroom, bathroom, image })
       .then(result => {
         res.send(result.insertedCount > 0)
-      }).catch(function (error) {
-        // Handle error
+      })
+      .catch(error => {
+        console.log(error);
       });
-  })
+  });
+
+  app.get('/apartments', (req, res) => {
+    apartmentCollection.find({})
+      .toArray((err, docs) => res.send(docs));
+  });
 
 });
 
